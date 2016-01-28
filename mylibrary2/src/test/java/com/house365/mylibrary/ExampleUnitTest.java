21d20a6dbd3cc05719fd36389dd8d233884dfd68
zhangzhenli <zhangzhenli@live.com>
2016-01-28 17:18:34 +0800

    添加额外的测试项目

---
+++ mylibrary2/src/test/java/com/house365/mylibrary/ExampleUnitTest.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2015-2016, House365. All rights reserved.
+ */
+
+package com.house365.mylibrary;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
