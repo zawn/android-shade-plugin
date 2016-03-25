9dd1b4face82f8143abe9cdbc6e098fe1afb0148
zhangzhenli <zhangzhenli@vip.house365.com>
2016-03-25 12:59:47 +0800

    排除本地依赖

--- src/main/groovy/com/house365/build/transform/ShadeJarTransform.groovy
+++ src/main/groovy/com/house365/build/transform/ShadeJarTransform.groovy
@@ -82,7 +82,7 @@ public class ShadeJarTransform extends Transform {
     @Override
     public Set<Scope> getScopes() {
         if (isLibrary) {
-            return Sets.immutableEnumSet(Scope.PROJECT, Scope.PROJECT_LOCAL_DEPS);
+            return Sets.immutableEnumSet(Scope.PROJECT);
         }
         return TransformManager.SCOPE_FULL_PROJECT;
     }
