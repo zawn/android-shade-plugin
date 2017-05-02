243ebb77d9f2f62b1c3d41191be9a3e060476f57
zhangzhenli <zhangzhenli@live.com>
2017-05-02 10:47:03 +0800

    修复Manifest合并过程中remove标签未去除.

--- src/main/groovy/com/house365/build/gradle/tasks/MergeShadeManifests.java
+++ src/main/groovy/com/house365/build/gradle/tasks/MergeShadeManifests.java
@@ -1,6 +1,7 @@
 package com.house365.build.gradle.tasks;
 
 import com.android.annotations.NonNull;
+import com.android.annotations.Nullable;
 import com.android.build.gradle.internal.dsl.CoreBuildType;
 import com.android.build.gradle.internal.dsl.CoreProductFlavor;
 import com.android.build.gradle.internal.scope.ConventionMappingHelper;
@@ -10,15 +11,21 @@ import com.android.build.gradle.internal.variant.ApkVariantOutputData;
 import com.android.build.gradle.internal.variant.BaseVariantData;
 import com.android.build.gradle.internal.variant.BaseVariantOutputData;
 import com.android.build.gradle.tasks.MergeManifests;
+import com.android.builder.core.AndroidBuilder;
 import com.android.builder.core.VariantConfiguration;
 import com.android.builder.model.ApiVersion;
 import com.android.manifmerger.ManifestMerger2;
 import com.android.manifmerger.ManifestProvider;
+import com.android.manifmerger.ManifestSystemProperty;
+import com.android.manifmerger.MergingReport;
 import com.google.common.collect.Lists;
 import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.commons.lang3.reflect.MethodInvokeUtils;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Callable;
 
 /**
@@ -30,7 +37,7 @@ public class MergeShadeManifests extends MergeManifests {
         File aaptManifestFile = getAaptFriendlyManifestOutputFile();
         String aaptFriendlyManifestOutputFilePath =
                 aaptManifestFile == null ? null : aaptManifestFile.getAbsolutePath();
-        getBuilder().mergeManifestsForApplication(
+        mergeManifestsForShadeLibrary(
                 getMainManifest(),
                 getManifestOverlays(),
                 getProviders(),
@@ -202,6 +209,147 @@ public class MergeShadeManifests extends MergeManifests {
             public String getName() {
                 return name;
             }
+        }
+    }
+
+
+    /**
+     * Invoke the Manifest Merger version 2.
+     *
+     * @see AndroidBuilder#mergeManifestsForApplication(File, List, List, String, int, String, String, String, Integer, String, String, String, ManifestMerger2.MergeType, Map, List, File)
+     */
+    public void mergeManifestsForShadeLibrary(
+            @NonNull File mainManifest,
+            @NonNull List<File> manifestOverlays,
+            @NonNull List<? extends ManifestProvider> dependencies,
+            String packageOverride,
+            int versionCode,
+            String versionName,
+            @Nullable String minSdkVersion,
+            @Nullable String targetSdkVersion,
+            @Nullable Integer maxSdkVersion,
+            @NonNull String outManifestLocation,
+            @Nullable String outAaptSafeManifestLocation,
+            @Nullable String outInstantRunManifestLocation,
+            ManifestMerger2.MergeType mergeType,
+            Map<String, Object> placeHolders,
+            @NonNull List<ManifestMerger2.Invoker.Feature> optionalFeatures,
+            @Nullable File reportFile) {
+
+        try {
+
+            ManifestMerger2.Invoker manifestMergerInvoker =
+                    ManifestMerger2.newMerger(mainManifest, getILogger(), mergeType)
+                            .setPlaceHolderValues(placeHolders)
+                            .addFlavorAndBuildTypeManifests(
+                                    manifestOverlays.toArray(new File[manifestOverlays.size()]))
+                            .addManifestProviders(dependencies)
+                            .withFeatures(optionalFeatures.toArray(
+                                    new ManifestMerger2.Invoker.Feature[optionalFeatures.size()]))
+                            .setMergeReportFile(reportFile);
+
+            manifestMergerInvoker.withFeatures(ManifestMerger2.Invoker.Feature.REMOVE_TOOLS_DECLARATIONS);
+
+            //noinspection VariableNotUsedInsideIf
+            if (outAaptSafeManifestLocation != null) {
+                manifestMergerInvoker.withFeatures(ManifestMerger2.Invoker.Feature.MAKE_AAPT_SAFE);
+            }
+
+            setInjectableValues(manifestMergerInvoker,
+                    packageOverride, versionCode, versionName,
+                    minSdkVersion, targetSdkVersion, maxSdkVersion);
+
+            MergingReport mergingReport = manifestMergerInvoker.merge();
+            getILogger().verbose("Merging result: %1$s", mergingReport.getResult());
+            switch (mergingReport.getResult()) {
+                case WARNING:
+                    mergingReport.log(getILogger());
+                    // fall through since these are just warnings.
+                case SUCCESS:
+                    String xmlDocument = mergingReport.getMergedDocument(
+                            MergingReport.MergedManifestKind.MERGED);
+                    String annotatedDocument = mergingReport.getMergedDocument(
+                            MergingReport.MergedManifestKind.BLAME);
+                    if (annotatedDocument != null) {
+                        getILogger().verbose(annotatedDocument);
+                    }
+                    save(xmlDocument, new File(outManifestLocation));
+                    getILogger().verbose("Merged manifest saved to " + outManifestLocation);
+
+                    if (outAaptSafeManifestLocation != null) {
+                        save(mergingReport.getMergedDocument(MergingReport.MergedManifestKind.AAPT_SAFE),
+                                new File(outAaptSafeManifestLocation));
+                    }
+
+                    if (outInstantRunManifestLocation != null) {
+                        String instantRunMergedManifest = mergingReport.getMergedDocument(
+                                MergingReport.MergedManifestKind.INSTANT_RUN);
+                        if (instantRunMergedManifest != null) {
+                            save(instantRunMergedManifest, new File(outInstantRunManifestLocation));
+                        }
+                    }
+                    break;
+                case ERROR:
+                    mergingReport.log(getILogger());
+                    throw new RuntimeException(mergingReport.getReportString());
+                default:
+                    throw new RuntimeException("Unhandled result type : "
+                            + mergingReport.getResult());
+            }
+        } catch (ManifestMerger2.MergeFailureException e) {
+            // TODO: unacceptable.
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Sets the {@link ManifestSystemProperty} that can be injected
+     * in the manifest file.
+     */
+    private static void setInjectableValues(
+            ManifestMerger2.Invoker<?> invoker,
+            String packageOverride,
+            int versionCode,
+            String versionName,
+            @Nullable String minSdkVersion,
+            @Nullable String targetSdkVersion,
+            @Nullable Integer maxSdkVersion) {
+
+        try {
+            MethodInvokeUtils.invokeStaticMethod(AndroidBuilder.class, "setInjectableValues",
+                    invoker,
+                    packageOverride,
+                    versionCode,
+                    versionName,
+                    minSdkVersion,
+                    targetSdkVersion,
+                    maxSdkVersion);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Saves the {@link com.android.manifmerger.XmlDocument} to a file in UTF-8 encoding.
+     *
+     * @param xmlDocument xml document to save.
+     * @param out         file to save to.
+     */
+    private static void save(String xmlDocument, File out) {
+        try {
+            MethodInvokeUtils.invokeStaticMethod(AndroidBuilder.class, "save",
+                    xmlDocument,
+                    out);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
         }
     }
 
